d0a6e0da6deb6893986a953edb0323b70b5d59bf
Vlad <vlad.arruda@gmail.com>
2026-05-21 13:17:11 -0300

    novas atualizações

--- cadastro-cartas-Vlad-Arruda-main/cadastro-cartas-Vlad-Arruda-main/CartasSuperTrunfo.c
+++ cadastro-cartas-Vlad-Arruda-main/cadastro-cartas-Vlad-Arruda-main/CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
 
-// Desafio Super Trunfo - Países
+//;// Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
